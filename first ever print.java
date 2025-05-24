61076379b14e2f2040b4b6f2387fd20ad312d61f
Harsh Raundal <dkingston0000@gmail.com>
2025-05-25 00:05:53 +0530

    Create first ever print.java

---
+++ first ever print.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("BlacklegSanji8\n");
+        System.out.println("BGMI player");
+        //this is a comment !! single line comment !
+        /*
+        this is a multiline comment
+        so this is the multiline
+         */
+        System.out.println("This is good language !\n");
+    }
+}
