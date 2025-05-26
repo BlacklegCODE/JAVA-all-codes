70af917b9509b1b84a81cae07390faf403f6bdd7
Harsh Raundal <dkingston0000@gmail.com>
2025-05-26 14:20:24 +0530

    Create learning operators.java

---
+++ learning operators.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Harsh
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 34;
+        int z = x*y;
+        //int add = x+y;
+        //int sub = x-y;
+        //int mod = x%y;
+        //int divide = x/y;
+
+//        System.out.println(z);
+
+
+        //augmented assignment operators :
+
+//        x += y;
+//        x -= y;
+//        x *=y;
+//        x /= y;
+//        x%= y;
+
+        //order of operations in java p - e- m - d - a - s
+//        parenthesis
+//        exponents
+//        multiplication
+//        addition
+//        substraction
+
+        
+        double result = 4+4*(7-5)/2.0;
+        System.out.println(result);
+    }
+}
