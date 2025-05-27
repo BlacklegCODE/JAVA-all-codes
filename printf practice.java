61fdcd91e315850cc533a661070d41c1c3281963
Harsh Raundal <dkingston0000@gmail.com>
2025-05-27 12:16:49 +0530

    Create printf practice.java

---
+++ printf practice.java
@@ -0,0 +1,20 @@
+
+public class Harsh
+{
+    public static void main(String[] agrs)
+    {
+        String name = "Luffy";
+        char firstletter = 'L';
+        int age = 20;
+        double height = 40.3;
+        boolean king = true;
+
+        System.out.printf("Hello %s\n",name);
+        System.out.printf("Your name starts with :%c\n",firstletter);
+        System.out.printf("Your age is :%d\n" ,age);
+        System.out.printf("You height is ;%f\n",height);
+        System.out.printf("Your status is ;%b\n",king);
+
+        System.out.printf("%s is %d years old",name,age);
+    }
+}
