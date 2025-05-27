8a002232666ece67deb48a0b4c6a33e5111f15d0
Harsh Raundal <dkingston0000@gmail.com>
2025-05-27 12:07:01 +0530

    Create Important math functions.java

---
+++ Important math functions.java
@@ -0,0 +1,29 @@
+public class Harsh
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+
+        double result;
+        result = Math.pow(2,3);
+
+        System.out.println(result);
+
+        result = Math.abs(-5);
+
+        result = Math.sqrt(9);
+
+        result = Math.round(3.14);
+
+        result = Math.ceil(3.14);
+
+        result = Math.floor(4.56);
+
+        result = Math.max(10,20);
+
+        result = Math.min(34,44);
+        System.out.println(result);
+    }
+}
