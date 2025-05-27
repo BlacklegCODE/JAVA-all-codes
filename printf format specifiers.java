1035375cfeb9efffa212ef8c6c28f15b4cdb5929
Harsh Raundal <dkingston0000@gmail.com>
2025-05-27 12:41:21 +0530

    Create printf format specifiers.java

---
+++ printf format specifiers.java
@@ -0,0 +1,15 @@
+public class Harsh
+{
+    public static void main(String[] args)
+    {
+        //precision practice using printf statement !!
+
+        double price1 = 90000.99;
+        double price2 = 1010000.4;
+        double price3 = -400000.3;
+
+        System.out.printf("%,.2f\n",price1);
+        System.out.printf("%,.1f\n",price2);
+        System.out.printf("%,.1f\n",price3);
+    }
+}
