e3fd860f33dc81877398a14c2f1a88b42142970a
Harsh Raundal <dkingston0000@gmail.com>
2025-05-28 09:30:57 +0530

    Create most important string methods practice.java

---
+++ most important string methods practice.java
@@ -0,0 +1,51 @@
+public class Harsh
+{
+    public static void main(String[] args)
+    {
+        String name = "Harsh";
+
+//        int length = name.length();
+//        char letter = name.charAt(0);
+//        System.out.println(length);
+//        System.out.println(letter);
+//        int index = name.indexOf("s");
+//        System.out.println(index);
+//        int lastIndex = name.lastIndexOf("H");
+//        System.out.println(lastIndex);
+//        name = name.toUpperCase();
+
+//        name = name.replace("s","a");
+//
+//        if(name.isEmpty())
+//        {
+//            System.out.println(" Your name field is empty !");
+//        }
+//        else
+//        {
+//            System.out.println("There is a name !");
+//        }
+//
+
+//        if(name.contains("H"))
+//        {
+//            System.out.println("the letter exists");
+//        }
+//        else
+//        {
+//            System.out.println("It doesnt exist !");
+//        }
+
+        if(name.equals("password"))
+        {
+            System.out.println("Your name cant be password !");
+        }
+        else
+        {
+            System.out.println("Hello "+name);
+        }
+
+        
+
+
+    }
+}
