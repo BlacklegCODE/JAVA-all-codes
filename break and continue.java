fb6873353bf645a8fc61126e2b66729071150c9e
Harsh Raundal <dkingston0000@gmail.com>
2025-05-29 21:38:30 +0530

    Create break and continue.java

---
+++ break and continue.java
@@ -0,0 +1,18 @@
+public class Harsh
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0;i<10;i++)
+        {
+            if(i == 5)
+            {
+                //break means stop on the current iteration
+                //break;
+
+                //continue is used to skip the current iteration, and continue further
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+    }
+}
