212db5eec0623989883d9dcd792f03150f521f25
Harsh Raundal <dkingston0000@gmail.com>
2026-09-20 13:20:14 +0530

    Remove unnecessary newline in sum function

---
+++ CircumferanceReturner.java
@@ -0,0 +1,20 @@
+import java.util.*;
+//Return sum of all odd numbers from 1 to n
+class first
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n :");
+        int n = sc.nextInt();
+
+        System.out.println(sum(n));
+    }
+
+    static double sum(int num)
+    {
+        double pi = 3.14;
+        double cir = 2 * pi * num;
+        return cir;
+    }
+}
